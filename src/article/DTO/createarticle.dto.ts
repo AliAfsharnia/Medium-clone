58731725fcf9a66ffd10150848bdfed1c54f41cd
AliAfsharnia <mr.ali007021@gmail.com>
2024-08-11 12:31:18 +0330

    Reapply "docs: add swagger to project"
    This reverts commit ab746628a0ab52b7f6d892f1e31358044e4570bb.

--- src/article/DTO/createarticle.dto.ts
+++ src/article/DTO/createarticle.dto.ts
@@ -1,14 +1,31 @@
+import { ApiProperty } from "@nestjs/swagger";
 import { IsNotEmpty } from "class-validator";
 
 export class CreateArticleDTO{
+    @ApiProperty({
+        example: 'our little lies',
+        required: true
+     })
     @IsNotEmpty()
     readonly title: string;
-
+    
+    @ApiProperty({
+        example: 'book for grown mans',
+        required: false
+     })
     @IsNotEmpty()
     readonly description: string;
 
+    @ApiProperty({
+        example: 'body',
+        required: false
+     })
     @IsNotEmpty()
     readonly body: string;
 
+    @ApiProperty({
+        example: 'story, study',
+        required: false
+     })
     readonly tagList?: string[];
 }
